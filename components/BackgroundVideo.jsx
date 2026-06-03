f4ba17af06e852e17f1c35dbd482859a5622e364
Riffard <riffardfunded@gmail.com>
2026-06-03 12:52:17 +0200

    Fix desktop layout

--- components/BackgroundVideo.jsx
+++ components/BackgroundVideo.jsx
@@ -1,12 +1,51 @@
 import { ResizeMode, Video } from "expo-av";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 
 export default function BackgroundVideo() {
+  if (Platform.OS === "web") {
+    return (
+      <View
+        pointerEvents="none"
+        style={[
+          StyleSheet.absoluteFillObject,
+          {
+            overflow: "hidden",
+            zIndex: -1,
+          },
+        ]}
+      >
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          style={{
+            position: "absolute",
+            inset: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
+        >
+          <source src="/welcome.mp4" type="video/mp4" />
+        </video>
+      </View>
+    );
+  }
+
   return (
-    <View style={StyleSheet.absoluteFill} pointerEvents="none">
+    <View
+      pointerEvents="none"
+      style={[
+        StyleSheet.absoluteFillObject,
+        {
+          overflow: "hidden",
+        },
+      ]}
+    >
       <Video
         source={require("../assets/welcome.mp4")}
-        style={StyleSheet.absoluteFill}
+        style={StyleSheet.absoluteFillObject}
         resizeMode={ResizeMode.COVER}
         shouldPlay
         isLooping
