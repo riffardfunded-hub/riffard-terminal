5d3dc58c35adb543d5fe0265a3937fb3ba806d8e
Riffard <riffardfunded@gmail.com>
2026-06-03 13:46:57 +0200

    Restore background video

--- components/BackgroundVideo.jsx
+++ components/BackgroundVideo.jsx
@@ -1,51 +1,12 @@
 import { ResizeMode, Video } from "expo-av";
-import { Platform, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function BackgroundVideo() {
-  if (Platform.OS === "web") {
-    return (
-      <View
-        pointerEvents="none"
-        style={[
-          StyleSheet.absoluteFillObject,
-          {
-            overflow: "hidden",
-            zIndex: -1,
-          },
-        ]}
-      >
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          style={{
-            position: "absolute",
-            inset: 0,
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-          }}
-        >
-          <source src="/welcome.mp4" type="video/mp4" />
-        </video>
-      </View>
-    );
-  }
-
   return (
-    <View
-      pointerEvents="none"
-      style={[
-        StyleSheet.absoluteFillObject,
-        {
-          overflow: "hidden",
-        },
-      ]}
-    >
+    <View style={StyleSheet.absoluteFill} pointerEvents="none">
       <Video
         source={require("../assets/welcome.mp4")}
-        style={StyleSheet.absoluteFillObject}
+        style={StyleSheet.absoluteFill}
         resizeMode={ResizeMode.COVER}
         shouldPlay
         isLooping
