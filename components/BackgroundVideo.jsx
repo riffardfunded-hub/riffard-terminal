a5569d0b03a8fae95e88ccbeb57e720142ded680
Riffard <riffardfunded@gmail.com>
2026-06-03 14:16:12 +0200

    Use fixed video on web

--- components/BackgroundVideo.jsx
+++ components/BackgroundVideo.jsx
@@ -1,11 +1,15 @@
 import { ResizeMode, Video } from "expo-av";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 
 export default function BackgroundVideo() {
   return (
     <View style={StyleSheet.absoluteFill} pointerEvents="none">
       <Video
-        source={require("../assets/welcome.mp4")}
+        source={
+          Platform.OS === "web"
+            ? require("../assets/welcome-fixed.mp4")
+            : require("../assets/welcome.mp4")
+        }
         style={StyleSheet.absoluteFill}
         resizeMode={ResizeMode.COVER}
         shouldPlay
